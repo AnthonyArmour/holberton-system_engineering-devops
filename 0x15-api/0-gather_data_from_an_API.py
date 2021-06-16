263c91852fb20a9b01247639da694fb351fb16fc
AnthonyArmour <anthonydarmour@gmail.com>
2021-06-16 19:12:50 +0000

    debug task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,14 +9,14 @@ if __name__ == "__main__":
     if len(argv) > 1:
         todo_dict = requests.get(todo, params={"userId": argv[1]}).json()
         user_dict = requests.get(user, params={"id": argv[1]}).json()
-        username = user_dict[0].get("name")
-    num_of_tasks = len(todo_dict)
-    done_tasks = 0
+        EMPLOYEE_NAME = user_dict[0].get("name")
+    TOTAL_NUMBER_OF_TASKS = len(todo_dict)
+    NUMBER_OF_DONE_TASKS = 0
     for dic in todo_dict:
         if dic["completed"] is True:
-            done_tasks += 1
+            NUMBER_OF_DONE_TASKS += 1
     print("Employee {} is done with tasks({}/{}):"
-          .format(username, done_tasks, num_of_tasks))
+          .format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
     for dic in todo_dict:
         if dic["completed"] is True:
             print("\t{}".format(dic["title"]))
